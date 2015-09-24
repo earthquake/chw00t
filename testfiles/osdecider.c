68c2782cd75739ecc1274320ec35acdafdc16a6a
earthquake <github@rycon.hu>
2015-09-24 22:43:36 +0200

    not to forget predefined symbols

---
+++ testfiles/osdecider.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+int main()
+{
+#if __linux__
+	printf("Linux\n");
+#endif
+#if __FreeBSD__
+	printf("FreeBSD\n");
+#endif
+#if __OpenBSD__
+	printf("OpenBSD\n");
+#endif
+#if __DragonflyBSD__
+	printf("DragonflyBSD\n");
+#endif
+#if __NetBSD__
+	printf("NetBSD\n");
+#endif
+#if __APPLE__
+	printf("Mac Os X\n");
+#endif
+#if __sun
+	printf("Solaris\n");
+#endif
+}	
